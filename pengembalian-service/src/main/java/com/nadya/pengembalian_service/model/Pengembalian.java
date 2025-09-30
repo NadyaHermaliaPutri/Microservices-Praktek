f4c9e4680a44a87b216468490ff3dceec404de10
NadyaHermaliaPutri <nadyahermaliaputri18@gmail.com>
2025-09-30 15:18:18 +0700

    fix: Memunculkan Hitungan Denda

--- pengembalian-service/src/main/java/com/nadya/pengembalian_service/model/Pengembalian.java
+++ pengembalian-service/src/main/java/com/nadya/pengembalian_service/model/Pengembalian.java
@@ -1,6 +1,6 @@
 package com.nadya.pengembalian_service.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+
 @Data
 @Entity
 @Table(name = "pengembalian")
@@ -18,7 +19,7 @@ public class Pengembalian {
     private Long bukuId;
     private Long anggotaId;
     private Long peminjamanId;
-    private Date tanggal_dikembalikan;
+    private LocalDate tanggal_dikembalikan;
     private String terlambat;
     private Double denda;
 }
